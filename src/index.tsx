bbde94a1201baa1aa01ae2c62616986535424748
Adam Gastineau <adam@agg.im>
2023-10-05 11:00:53 -0700

    Proper display of the image grid

--- src/index.tsx
+++ src/index.tsx
@@ -4,16 +4,23 @@ import { App } from "./react/App";
 
 console.log("injecting react");
 
-const reactRoot = document.createElement("div");
+const id = "bing-dalle3-react-root";
 
-reactRoot.style.position = "fixed";
-reactRoot.style.top = "0";
-reactRoot.style.bottom = "0";
-reactRoot.style.left = "0";
-reactRoot.style.right = "0";
-reactRoot.style.zIndex = "100";
-reactRoot.style.background = "rgba(255,255,255,0.5)";
+let reactRoot = document.getElementById(id);
 
-document.body.append(reactRoot);
+if (!reactRoot) {
+  reactRoot = document.createElement("div");
+  reactRoot.id = id;
+
+  reactRoot.style.position = "fixed";
+  reactRoot.style.top = "0";
+  reactRoot.style.bottom = "0";
+  reactRoot.style.left = "0";
+  reactRoot.style.right = "0";
+  reactRoot.style.zIndex = "100";
+  reactRoot.style.background = "rgba(255,255,255,0.5)";
+
+  document.body.append(reactRoot);
+}
 
 createRoot(reactRoot).render(<App />);
--- src/react/components/Generation.tsx
+++ src/react/components/Generation.tsx
@@ -3,6 +3,8 @@ import { PromptDBEntry } from "../../content/db";
 import { useGenerationImages } from "../hooks/useGenerationImages";
 import { formatDatetimeNice } from "../../util/string";
 
+import classes from "./Generation.module.css";
+
 interface GenerationProps {
   prompt: PromptDBEntry;
 }
@@ -15,24 +17,52 @@ export const Generation: React.FC<GenerationProps> = ({ prompt }) => {
     [images]
   );
 
+  const imgElements = imageUrls.map((url) => (
+    <div className={classes.imageWrapper}>
+      <img key={url} className={classes.image} src={url} />
+    </div>
+  ));
+
+  const row0: React.ReactNode[] = [];
+  const row1: React.ReactNode[] = [];
+  for (let i = 0; i < imgElements.length; i++) {
+    const element = imgElements[i];
+
+    if (i > 1) {
+      row1.push(element);
+    } else {
+      row0.push(element);
+    }
+  }
+
+  const imageGrid =
+    imgElements.length === 2
+      ? `${classes.imageGrid} ${classes.twoItemImageGrid}`
+      : classes.imageGrid;
+
   return (
-    <div>
-      <div>{prompt.prompt}</div>
-      <div>{formatDatetimeNice(new Date(prompt.recordTimestamp))}</div>
-      <div
-        style={{
-          display: "grid",
-          gridTemplateColumns: "repeat(2, 200px)",
-          gap: 16,
-        }}
-      >
-        {imageUrls.map((url) => (
-          <img
-            key={url}
-            style={{ width: "100%", maxHeight: "100%" }}
-            src={url}
-          />
-        ))}
+    <div className={classes.wrapper}>
+      <div className={imageGrid}>
+        <div className={classes.row}>
+          {row0.map((img, i) => (
+            <div key={i} className={classes.rowItem}>
+              {img}
+            </div>
+          ))}
+        </div>
+        {row1.length > 0 && (
+          <div className={classes.row}>
+            {row1.map((img, i) => (
+              <div key={i} className={classes.rowItem}>
+                {img}
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+      <div className={classes.controls}>
+        <div>{prompt.prompt}</div>
+        <div>{formatDatetimeNice(new Date(prompt.recordTimestamp))}</div>
       </div>
     </div>
   );
